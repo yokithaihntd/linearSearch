a6c6263e8e3a49ebe3a2f4595dc595d840962a73
Macsim <macsim.bilozub@gmail.com>
2024-04-15 21:56:01 +0300

    Добавьте файлы проекта.

---
+++ Practical7/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskManager taskManager = new TaskManager();
+            taskManager.AddTask(new Task("Задача 1", "Открыта"));
+            taskManager.AddTask(new Task("Задача 2", "Закрыта"));
+            taskManager.AddTask(new Task("Задача 3", "Открыта"));
+            taskManager.AddTask(new Task("Задача 4", "В процессе"));
+            taskManager.AddTask(new Task("Задача 5", "Закрыта"));
+            taskManager.AddTask(new Task("Задача 6", "В процессе"));
+
+            bool exitRequested = false;
+
+            while (!exitRequested)
+            {
+                Console.WriteLine("Введите статус задачи для поиска:");
+                string statusToSearch = Console.ReadLine();
+
+                List<Task> tasksFound = taskManager.FindTasksByStatus(statusToSearch);
+
+                if (tasksFound.Count > 0)
+                {
+                    Console.WriteLine("Найденные задачи со статусом '" + statusToSearch + "':");
+                    foreach (var task in tasksFound)
+                    {
+                        Console.WriteLine(task.Name);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Задачи со статусом '" + statusToSearch + "' не найдены.");
+                }
+
+                Console.WriteLine("Нажмите Enter для продолжения поиска или 'q' для выхода...");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "q")
+                {
+                    exitRequested = true;
+                }
+
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ Practical7/TaskManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical7
+{
+    class TaskManager
+    {
+        private List<Task> tasks;
+
+        public TaskManager()
+        {
+            tasks = new List<Task>();
+        }
+
+        public void AddTask(Task task)
+        {
+            tasks.Add(task);
+        }
+
+        public List<Task> FindTasksByStatus(string status)
+        {
+            List<Task> foundTasks = new List<Task>();
+
+            foreach (var task in tasks)
+            {
+                if (task.Status == status)
+                {
+                    foundTasks.Add(task);
+                }
+            }
+
+            return foundTasks;
+        }
+    }
+}
